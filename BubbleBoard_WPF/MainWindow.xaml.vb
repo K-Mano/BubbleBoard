b56b974eaeabf57d3c08f1823505b27e4e69d025
K-Mano <39230424+K-Mano@users.noreply.github.com>
2019-02-28 23:34:37 +0900

    Blur is Difficult

--- BubbleBoard_WPF/MainWindow.xaml.vb
+++ BubbleBoard_WPF/MainWindow.xaml.vb
@@ -4,80 +4,70 @@ Imports System.Runtime.InteropServices
 Imports System.Text
 Imports System.Windows.Interop
 Imports System.Windows.Media.Animation
+<StructLayout(LayoutKind.Sequential)>
+Friend Structure WindowCompositionAttributeData
+    Public Attribute As WindowCompositionAttribute
+    Public Data As IntPtr
+    Public SizeOfData As Integer
+End Structure
+
+Friend Enum WindowCompositionAttribute
+    WCA_ACCENT_POLICY = 19
+End Enum
 
 Friend Enum AccentState
-        ACCENT_DISABLED = 0
-        ACCENT_ENABLE_GRADIENT = 1
-        ACCENT_ENABLE_TRANSPARENTGRADIENT = 2
-        ACCENT_ENABLE_BLURBEHIND = 3
-        ACCENT_ENABLE_ACRYLICBLURBEHIND = 4
-        ACCENT_INVALID_STATE = 5
-    End Enum
+    ACCENT_DISABLED = 0
+    ACCENT_ENABLE_GRADIENT = 1
+    ACCENT_ENABLE_TRANSPARENTGRADIENT = 2
+    ACCENT_ENABLE_BLURBEHIND = 3
+    ACCENT_INVALID_STATE = 4
+End Enum
 
-    <StructLayout(LayoutKind.Sequential)>
-    Friend Structure AccentPolicy
-        Public AccentState As AccentState
-        Public AccentFlags As UInteger
-        Public GradientColor As UInteger
-        Public AnimationId As UInteger
-    End Structure
+<StructLayout(LayoutKind.Sequential)>
+Friend Structure AccentPolicy
+    Public AccentState As AccentState
+    Public AccentFlags As Integer
+    Public GradientColor As UInteger
+    Public AnimationId As Integer
+End Structure
 
-    <StructLayout(LayoutKind.Sequential)>
-    Friend Structure WindowCompositionAttributeData
-        Public Attribute As WindowCompositionAttribute
-        Public Data As IntPtr
-        Public SizeOfData As Integer
-    End Structure
-
-    Friend Enum WindowCompositionAttribute
-        WCA_ACCENT_POLICY = 19
-    End Enum
 
 Partial Public Class MainWindow
     Inherits Window
+    Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Public Overrides Sub OnApplyTemplate()
+            MyBase.OnApplyTemplate()
+            EnableBlur(Me)
+        End Sub
 
     <DllImport("user32.dll")>
     Friend Shared Function SetWindowCompositionAttribute(ByVal hwnd As IntPtr, ByRef data As WindowCompositionAttributeData) As Integer
 
     End Function
-    Private _blurOpacity As UInteger
 
-    Public Property BlurOpacity As Double
-        Get
-            Return _blurOpacity
-        End Get
-        Set(ByVal value As Double)
-            _blurOpacity = CUInt(value)
-            EnableBlur()
-        End Set
-    End Property
-
-    Private _blurBackgroundColor As UInteger = &H0
-
-    Public Sub New()
-        DataContext = Me
-    End Sub
-
-    Friend Sub EnableBlur()
-        Dim windowHelper = New WindowInteropHelper(Me)
-        Dim accent = New AccentPolicy With {
-                .AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND,
-                .GradientColor = &H66A5D2E6UI
-            }
+    Friend Shared Sub EnableBlur(ByVal win As Window)
+        Dim windowHelper = New WindowInteropHelper(win)
+        Dim accent = New AccentPolicy()
         Dim accentStructSize = Marshal.SizeOf(accent)
+        accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND
+        accent.AccentFlags = 2
+        accent.GradientColor = &H44FFFFBBUI
         Dim accentPtr = Marshal.AllocHGlobal(accentStructSize)
         Marshal.StructureToPtr(accent, accentPtr, False)
         Dim data = New WindowCompositionAttributeData With {
-                .Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY,
-                .SizeOfData = accentStructSize,
-                .Data = accentPtr
-            }
+            .Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY,
+            .SizeOfData = accentStructSize,
+            .Data = accentPtr
+        }
         SetWindowCompositionAttribute(windowHelper.Handle, data)
         Marshal.FreeHGlobal(accentPtr)
     End Sub
     Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs) Handles Me.Loaded
         NotiViewBackground.Visibility = Visibility.Hidden
-        EnableBlur()
+        EnableBlur(Me)
     End Sub
 
     Private Sub ApplyButton_Clicked(sender As Object, e As RoutedEventArgs) Handles ApplyButton.Click
