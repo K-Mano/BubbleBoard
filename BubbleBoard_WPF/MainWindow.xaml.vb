ef14e3ec3e845e11f5b81a95ed83b9622bc7ea4d
K-Mano <39230424+K-Mano@users.noreply.github.com>
2019-02-28 23:10:07 +0900

    BubbleBoard-Chromium1.1

--- BubbleBoard_WPF/MainWindow.xaml.vb
+++ BubbleBoard_WPF/MainWindow.xaml.vb
@@ -3,94 +3,162 @@ Imports System.IO
 Imports System.Runtime.InteropServices
 Imports System.Text
 Imports System.Windows.Interop
+Imports System.Windows.Media.Animation
 
-Namespace MainWindow
-    Class MainWindow
+Friend Enum AccentState
+        ACCENT_DISABLED = 0
+        ACCENT_ENABLE_GRADIENT = 1
+        ACCENT_ENABLE_TRANSPARENTGRADIENT = 2
+        ACCENT_ENABLE_BLURBEHIND = 3
+        ACCENT_ENABLE_ACRYLICBLURBEHIND = 4
+        ACCENT_INVALID_STATE = 5
+    End Enum
 
-        <DllImport("user32.dll")>
-        Friend Shared Function SetWindowCompositionAttribute(ByVal hwnd As IntPtr, ByRef data As WindowCompositionAttributeData) As Integer
+    <StructLayout(LayoutKind.Sequential)>
+    Friend Structure AccentPolicy
+        Public AccentState As AccentState
+        Public AccentFlags As UInteger
+        Public GradientColor As UInteger
+        Public AnimationId As UInteger
+    End Structure
 
-        End Function
+    <StructLayout(LayoutKind.Sequential)>
+    Friend Structure WindowCompositionAttributeData
+        Public Attribute As WindowCompositionAttribute
+        Public Data As IntPtr
+        Public SizeOfData As Integer
+    End Structure
 
-        Private Sub ApplyButton_Clicked(sender As Object, e As RoutedEventArgs) Handles ApplyButton.Click
-            If TitleBox.Text.Length > 0 And LinkBox.Text.Length > 0 Then
-                Try
-                    If Directory.Exists(".\bubbles") Then
+    Friend Enum WindowCompositionAttribute
+        WCA_ACCENT_POLICY = 19
+    End Enum
 
-                    Else
-                        Directory.CreateDirectory(".\bubbles")
-                    End If
-                    Directory.CreateDirectory(".\bubbles\Bubble_" + TitleBox.Text)
-                    File.Copy(".\Bubbles.exe", ".\bubbles\Bubble_" + TitleBox.Text + "\Bubble_" + TitleBox.Text + ".exe", True)
-                    Dim setlink As New StreamWriter(".\bubbles\Bubble_" + TitleBox.Text + "\Bubble_" + TitleBox.Text + ".dat", False, Encoding.GetEncoding("shift_jis"))
-                    setlink.Write(LinkBox.Text)
-                    setlink.Close()
-                    MessageBox.Show("Creation Succeed.", "Information", MessageBoxButton.OK, MessageBoxImage.Information)
-                Catch ex As Exception
-                    MessageBox.Show("Failed to Copying Files.", "Error", MessageBoxButton.OK, MessageBoxImage.Error)
-                End Try
-            Else
-                MessageBox.Show("Input the Title and Link.", "Error", MessageBoxButton.OK, MessageBoxImage.Error)
-            End If
-        End Sub
+Partial Public Class MainWindow
+    Inherits Window
 
-        Private Sub GlassLec_LeftDown(sender As Object, e As MouseButtonEventArgs) Handles GlassBorder.MouseLeftButtonDown
-            If e.ButtonState <> MouseButtonState.Pressed Then Return
-            DragMove()
-        End Sub
+    <DllImport("user32.dll")>
+    Friend Shared Function SetWindowCompositionAttribute(ByVal hwnd As IntPtr, ByRef data As WindowCompositionAttributeData) As Integer
 
-        <StructLayout(LayoutKind.Sequential)>
-        Friend Structure WindowCompositionAttributeData
-            Public Attribute As WindowCompositionAttribute
-            Public Data As IntPtr
-            Public SizeOfData As Integer
-        End Structure
+    End Function
+    Private _blurOpacity As UInteger
 
-        Friend Enum WindowCompositionAttribute
-            WCA_ACCENT_POLICY = 19
-        End Enum
+    Public Property BlurOpacity As Double
+        Get
+            Return _blurOpacity
+        End Get
+        Set(ByVal value As Double)
+            _blurOpacity = CUInt(value)
+            EnableBlur()
+        End Set
+    End Property
 
-        Friend Enum AccentState
-            ACCENT_DISABLED = 0
-            ACCENT_ENABLE_GRADIENT = 1
-            ACCENT_ENABLE_TRANSPARENTGRADIENT = 2
-            ACCENT_ENABLE_BLURBEHIND = 3
-            ACCENT_INVALID_STATE = 4
-        End Enum
+    Private _blurBackgroundColor As UInteger = &H0
 
-        <StructLayout(LayoutKind.Sequential)>
-        Friend Structure AccentPolicy
-            Public AccentState As AccentState
-            Public AccentFlags As Integer
-            Public GradientColor As UInteger
-            Public AnimationId As Integer
-        End Structure
-    End Class
-    Partial Public Class MainWindow
-        Inherits Window
+    Public Sub New()
+        DataContext = Me
+    End Sub
 
-        Public Overrides Sub OnApplyTemplate()
-            MyBase.OnApplyTemplate()
-            EnableBlur(Me)
-        End Sub
-
-        Friend Shared Sub EnableBlur(ByVal win As Window)
-            Dim windowHelper = New WindowInteropHelper(win)
-            Dim accent = New AccentPolicy()
-            Dim accentStructSize = Marshal.SizeOf(accent)
-            accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND
-            accent.AccentFlags = 2
-            accent.GradientColor = &H99222222UI
-            Dim accentPtr = Marshal.AllocHGlobal(accentStructSize)
-            Marshal.StructureToPtr(accent, accentPtr, False)
-            Dim data = New WindowCompositionAttributeData With {
+    Friend Sub EnableBlur()
+        Dim windowHelper = New WindowInteropHelper(Me)
+        Dim accent = New AccentPolicy With {
+                .AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND,
+                .GradientColor = &H66A5D2E6UI
+            }
+        Dim accentStructSize = Marshal.SizeOf(accent)
+        Dim accentPtr = Marshal.AllocHGlobal(accentStructSize)
+        Marshal.StructureToPtr(accent, accentPtr, False)
+        Dim data = New WindowCompositionAttributeData With {
                 .Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY,
                 .SizeOfData = accentStructSize,
                 .Data = accentPtr
             }
-            SetWindowCompositionAttribute(windowHelper.Handle, data)
-            Marshal.FreeHGlobal(accentPtr)
-        End Sub
-    End Class
-End Namespace
+        SetWindowCompositionAttribute(windowHelper.Handle, data)
+        Marshal.FreeHGlobal(accentPtr)
+    End Sub
+    Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs) Handles Me.Loaded
+        NotiViewBackground.Visibility = Visibility.Hidden
+        EnableBlur()
+    End Sub
+
+    Private Sub ApplyButton_Clicked(sender As Object, e As RoutedEventArgs) Handles ApplyButton.Click
+        If TitleBox.Text.Length > 0 And LinkBox.Text.Length > 0 Then
+            If LinkBox.Text.Contains("https://") Or LinkBox.Text.Contains("http://") Then
+                Try
+#Disable Warning BC42025 ' インスタンスを経由する共有メンバー、定数メンバー、列挙型メンバー、または入れ子にされた型へのアクセスです
+                    If Directory.Exists(".\WorkDirs") Then
+
+                    Else
+                        Directory.CreateDirectory(".\WorkDirs")
+                    End If
+                    Directory.CreateDirectory(".\WorkDirs\" + TitleBox.Text)
+                    Dim shortcutPath As String = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), TitleBox.Text + ".lnk")
+                    Dim targetPath As String = Path.GetDirectoryName(Reflection.Assembly.GetExecutingAssembly().Location) + "\Resources\Bubbles.exe"
+                    Dim t As Type = Type.GetTypeFromCLSID(New Guid("72C24DD5-D70A-438B-8A42-98424B88AFB8"))
+                    Dim shell = Activator.CreateInstance(t)
+                    Dim shortcut = shell.CreateShortcut(shortcutPath)
+                    shortcut.TargetPath = targetPath
+                    shortcut.Arguments = LinkBox.Text
+                    shortcut.WorkingDirectory = Path.GetDirectoryName(Reflection.Assembly.GetExecutingAssembly().Location) + "\WorkDirs\" + TitleBox.Text
+                    'shortcut.IconLocation = Application.ExecutablePath + ",0"
+                    shortcut.Save()
+                    Marshal.FinalReleaseComObject(shortcut)
+                    Marshal.FinalReleaseComObject(shell)
+                    TitleText.Text = "情報"
+                    Description.Text = "作成に成功しました。"
+                    NotiViewAnimationStartup()
+                Catch ex As Exception
+                    TitleText.Text = "ファイル操作"
+                    Description.Text = "ショートカットの作成に失敗しました。"
+                    NotiViewAnimationStartup()
+                End Try
+            Else
+                TitleText.Text = "作成失敗"
+                Description.Text = "リンクには""https://""または""http://""を含む必要があります。"
+                NotiViewAnimationStartup()
+            End If
+        Else
+            NotiViewBackground.Opacity = 0
+            TitleText.Text = "作成失敗"
+            Description.Text = "タイトルとリンクを入力してください。"
+            NotiViewAnimationStartup()
+        End If
+    End Sub
+
+    Private Sub GlassLec_LeftDown(sender As Object, e As MouseButtonEventArgs) Handles GlassBorder.MouseLeftButtonDown
+        If e.ButtonState <> MouseButtonState.Pressed Then Return
+        DragMove()
+    End Sub
+    Private Sub NotiViewAnimationStartup()
+        NotiViewBackground.Visibility = Visibility.Visible
+        NotiViewBackground.BeginAnimation(OpacityProperty, New DoubleAnimation(1, TimeSpan.FromSeconds(0.2)))
+        BlurEffect.BeginAnimation(BlurEffect.RadiusProperty, animation:=New DoubleAnimation(30, TimeSpan.FromSeconds(0.2)))
+    End Sub
+    Private Async Sub NotiViewAnimationShutDown()
+        NotiViewBackground.BeginAnimation(OpacityProperty, New DoubleAnimation(0, TimeSpan.FromSeconds(0.2)))
+        BlurEffect.BeginAnimation(BlurEffect.RadiusProperty, New DoubleAnimation(0, TimeSpan.FromSeconds(0.2)))
+        Await Task.Delay(200)
+        NotiViewBackground.Visibility = Visibility.Hidden
+    End Sub
+    Private Sub OKButton_Click(sender As Object, e As RoutedEventArgs) Handles OKButton.Click
+        NotiViewAnimationShutDown()
+    End Sub
+
+    Private Sub NotiView_LeftDown(sender As Object, e As MouseButtonEventArgs) Handles NotiViewBackground.MouseLeftButtonDown
+        If e.ButtonState <> MouseButtonState.Pressed Then Return
+        DragMove()
+    End Sub
+
+    Private Function ConvertHexToColor(hex As String) As Color
+        Dim color As Object = ColorConverter.ConvertFromString(hex)
+        Return color
+    End Function
+
+    Private Sub OKButton_MouseEnter(sender As Object, e As MouseEventArgs) Handles OKButton.MouseEnter
+        OKButtonText.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, New ColorAnimation(ConvertHexToColor("#FF464646"), TimeSpan.FromSeconds(0.1)))
+    End Sub
+
+    Private Sub OKButton_MouseLeave(sender As Object, e As MouseEventArgs) Handles OKButton.MouseLeave
+        OKButtonText.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, New ColorAnimation(ConvertHexToColor("#FFFFFFFF"), TimeSpan.FromSeconds(0.1)))
+    End Sub
+End Class
 
